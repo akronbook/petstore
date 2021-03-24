e4cb46f1fb6d2e404c0b42996bfa6e1f7c38fbd6
Yuanming Chen <yuanming@hotmail.com>
2021-03-23 23:35:42 -0600

    Created initializer

--- pet-store-app/src/app/app.module.ts
+++ pet-store-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreNavComponent } from './store-nav/store-nav.component';
 import { AdminNavComponent } from './admin-nav/admin-nav.component';
+import { initializer } from './initializer.service';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,17 @@ import { AdminNavComponent } from './admin-nav/admin-nav.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      deps: [KeycloakService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- pet-store-app/src/environments/environment.prod.ts
+++ pet-store-app/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  keycloak: {
+    issuer: 'http://localhost:8081/auth/',
+    realm: 'petshop-realm',
+    clientId: 'petstore-portal'
+  }
 };
