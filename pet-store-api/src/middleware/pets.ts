3ffe9999064b2af53bd5d028c7ab53212b3ee5ee
Yuanming Chen <yuanming@hotmail.com>
2021-03-24 00:47:19 -0600

    Get pets

--- pet-store-api/src/middleware/pets.ts
+++ pet-store-api/src/middleware/pets.ts
@@ -1,9 +1,12 @@
 import * as Koa from "koa";
+import Pet from "../model/pet";
 
 export async function GetPets(ctx: Koa.Context, next: () => Promise<any>): Promise<any> {
-    ctx.body = {
-        type: 'pets'
-    };
+    const pets: Array<Pet> = new Array<Pet>();
+    pets.push(new Pet(1, `Dog`, 'Oliver', 6));
+    pets.push(new Pet(2, `Cat`, 'Buster', 1));
+    pets.push(new Pet(3, `Bird`, 'Violet', 2));
+    ctx.body = pets;
     
     await next();
 }
---
+++ pet-store-api/src/model/pet.ts
@@ -0,0 +1,4 @@
+export default class Pet {
+    public constructor(public pet_id: number, public pet_type: string, public pet_name: string, public pet_age: number) {
+    }
+}
