91446b8af7c1abcd69f172d63c75c2c6ebb48653
Yuanming Chen <yuanming@hotmail.com>
2021-03-24 00:53:16 -0600

    Added transactions

--- pet-store-api/src/middleware/transactions.ts
+++ pet-store-api/src/middleware/transactions.ts
@@ -1,9 +1,13 @@
 import * as Koa from "koa";
+import Transaction from "../model/transaction";
+import uuid from "../keycloak-koa-connect/uuid";
 
 export async function GetTransactions(ctx: Koa.Context, next: () => Promise<any>): Promise<any> {
-    ctx.body = {
-        type: 'transactions'
-    };
+    const transactions: Array<Transaction> = new Array<Transaction>();
+    transactions.push(new Transaction(12.56, new Date().toISOString(), uuid(), `Cash`));
+    transactions.push(new Transaction(56.16, new Date().toISOString(), uuid(), `Credit card`));
+    transactions.push(new Transaction(88.99, new Date().toISOString(), uuid(), `Bitcoin`));
+    ctx.body = transactions;
     
     await next();
 }
---
+++ pet-store-api/src/model/transaction.ts
@@ -0,0 +1,4 @@
+export default class Transaction {
+    public constructor(public txn_amount: number, public txn_date: string, public txn_id: string, public txn_method: string) {
+    }
+}
